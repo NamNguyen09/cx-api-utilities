0a0ea7bf35f766d0676fe568d4ac3d91d9b4f9ee
NamNguyen09 <nam.brucelee15987@gmail.com>
2022-12-16 15:00:24 +0700

    Add cx.API.Infrastructure that supports for NET48

---
+++ cx.API.Infrastructure/Configurations/ConfigurationExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace cx.API.Infrastructure.Configurations
+{
+    public static class ConfigurationExtension
+    {
+        public static string ExpandEnvironmentVariable(this string variable)
+        {
+            if (string.IsNullOrEmpty(variable)) return "";
+
+            return Environment.ExpandEnvironmentVariables(variable);
+        }
+    }
+}
